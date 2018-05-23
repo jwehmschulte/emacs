377d6e8bb0601b37efab7da4dce90399edd2873f
jwehmschulte <julien.wehmschulte@yahoo.de>
2018-05-23 23:01:25 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,19 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
