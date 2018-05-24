75e8e4e0f40c5b06bc92d2b7f39c3b5354068c96
jwehmschulte <julien.wehmschulte@yahoo.de>
2018-05-25 00:03:15 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,16 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
-(use-package 
- :ensure t
- :init
- ())
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "FreeMono")))))
+
